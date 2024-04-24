7451486a6042284cecb31ac7e71d908ec253e79b
Youssef Alsarakibi <alsarakibiy@gmail.com>
2024-04-24 13:59:00 +0300

    added image to the mobile view landing

--- src/components/common/Landing.jsx
+++ src/components/common/Landing.jsx
@@ -10,17 +10,25 @@ import {
 } from "@/constants/routes";
 const slides = [
   {
-    videoUrl: "/video-1.mp4",
-    mobileUrl: "/mobile-2.mp4",
+    desktop: {
+      url: "/video-1.mp4",
+      img: false,
+    },
     mobile: {
-      width: 480,
+      url: "/mobile-1.jpeg",
+      img: true,
+      width: 470,
       height: 848,
     },
   },
   {
-    imgUrl: "/landing-bg.webp",
-    mobileUrl: "/mobile-2.mp4",
+    desktop: {
+      url: "/landing-bg.webp",
+      img: true,
+    },
     mobile: {
+      url: "/mobile-2.JPG",
+      img: true,
       width: 448,
       height: 848,
     },
@@ -72,7 +80,7 @@ const VideoSlider = () => {
         minHeight: isSmall
           ? calcVideoHeight({
               width: 480,
-              height: 848,
+              height: 840,
             })
           : "auto",
         backgroundColor: "#333",
@@ -89,11 +97,59 @@ const VideoSlider = () => {
       >
         {slides.map((slide, index) => (
           <div key={index} style={{ position: "relative" }}>
-            {slide.videoUrl || isSmall ? (
+            {isSmall ? (
+              slide.mobile.img ? (
+                <div>
+                  <img src={slide.mobile.url} style={{ width: "100%" }} />
+                </div>
+              ) : (
+                <>
+                  <ReactPlayer
+                    playIcon={<></>}
+                    ref={playerRef}
+                    url={slide.mobile.url}
+                    controls={false}
+                    loop={true}
+                    config={{
+                      file: {
+                        attributes: {
+                          style: {
+                            width: isBig ? "100vw" : "100%", // Override the width of the inner video element
+                          },
+                        },
+                      },
+                    }}
+                    muted={true}
+                    playing={true}
+                    width={"100vw"}
+                    height="auto"
+                    style={{
+                      position: currentSlide === index ? "relative" : "initial",
+                      zIndex: currentSlide === index ? "1" : "0",
+                    }}
+                    playsinline
+                  />
+                  <div style={{ display: "none" }}>
+                    <video
+                      controls={false}
+                      ref={(video) => (playerRef.current = video)}
+                      width={isSmall ? "auto" : "100%"}
+                    >
+                      <source src={slide.videoUrl} type="video/mp4" />
+                    </video>
+                  </div>
+                </>
+              )
+            ) : slide.desktop.img ? (
+              <div>
+                <img src={slide.desktop.url} style={{ width: "100%" }} />
+              </div>
+            ) : (
               <>
                 <ReactPlayer
+                  playIcon={<></>}
                   ref={playerRef}
-                  url={isSmall ? slide.mobileUrl : slide.videoUrl}
+                  url={slide.desktop.url}
                   controls={false}
                   loop={true}
                   config={{
@@ -125,10 +181,6 @@ const VideoSlider = () => {
                   </video>
                 </div>
               </>
-            ) : (
-              <div>
-                <img src={slide.imgUrl} style={{ width: "100%" }} />
-              </div>
             )}
 
             <div
@@ -136,7 +188,7 @@ const VideoSlider = () => {
               style={{
                 position: "absolute",
                 left: `${30 + 20 * index}%`,
-                top: "70%",
+                top: "90%",
                 translate: "-50% -50%",
                 zIndex: 2,
               }}
